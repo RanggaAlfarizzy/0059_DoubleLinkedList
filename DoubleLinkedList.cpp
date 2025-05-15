2028e4da08ee8616067f6dce6f559938c65470f3
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-15 20:46:01 +0800

    Menelusuri posisi berdasarkan nim dan mencegah duplikat

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -57,4 +57,18 @@ public:
         START = newNode;
         return;
     }
+
+    // insert in between node
+        // step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
+        }
 };
